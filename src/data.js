63ed0925463592255e55e869b9ef1628ad5ce011
Ritesh Dhurve <riteshdhurve2440@gmail.com>
2025-08-26 21:34:32 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,35 +4,36 @@ export const products = [
     cname: "POCO X4",
     company: "Xiaomi",
     price: 13999,
-    image: "poco.jpg"
+    image: "./images/poco.jpg"
   },
   {
     category: "mobile",
     cname: "Redmi Note 4",
     company: "Xiaomi",
     price: 9999,
-    image: "redmi.jpg"
+    image: "./images/redmi.jpg"
   },
   {
     category: "mobile",
     cname: "POCO X5",
     company: "Xiaomi",
     price: 15999,
-    image: "pocox5.jpg"
+    image: "./images/pocox5.jpg"
   },
   {
     category: "Laptop",
     cname: "IdeaPad Gaming",
     company: "Lenovo",
     price: 49999,
-    image: "ideapad.jpg"
+    image: "./images/ideapad.jpg"
   },
   {
     category: "Laptop",
     cname: "Yoga Slim",
     company: "Lenovo",
     price: 59999,
-    image: "yoga."
+    image: "./images/yoga."
   }
 ];
+
 
